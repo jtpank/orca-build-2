42cdb6ac62f1e2c4b9d4daa0a052e67117fed818
Justin <jtpank34@gmail.com>
2023-05-07 15:49:20 -0700

    first

---
+++ front-end/src/routes/Splash.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {Outlet} from 'react-router-dom';
+import '../styles/styles.css';
+
+class Splash extends React.Component {
+    // {this.props.is_logged_in ? (<h1>Welcome {this.props.email} </h1>) : (<h1>Welcome </h1>)}
+    render() {
+        return (
+        <div className=''>
+            <div className=''>
+                
+            </div>
+            
+            <div>
+            </div>
+
+            <div className='centered-div '>
+            <p>Welcome to main splash! </p>
+            </div>
+
+            <div className="row-splash">
+            <Outlet></Outlet>
+            </div>
+        </div>
+        );
+    }
+}
+
+export default Splash;
