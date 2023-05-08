37b68ce8a53d5a0575dd5c49332a7b59aecd2eac
Justin Pank <justinpank@Justins-MacBook-Pro-2.local>
2023-05-07 17:47:07 -0700

    small styles

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes} from 'react-router-dom';
 import Header from "./routes/Header";
 import Splash from "./routes/Splash";
 import Footer from "./routes/Footer";
+import './styles/styles.css';
 class App extends React.Component {
   constructor(props){
     super(props);
