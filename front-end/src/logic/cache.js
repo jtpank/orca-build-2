18788323b59a41aba6dd5be3d32aa65f3070f88a
Justin <jtpank34@gmail.com>
2023-05-12 10:50:05 -0700

    update TODO cache implementation as separate function

---
+++ front-end/src/logic/cache.js
@@ -0,0 +1,11 @@
+//TODO:
+// Setup isolated caching functionality to reduce code bloat
+export default function setCache(key, value) {
+    //store cache
+}
+export default function getCache(key, value) {
+    //store cache
+}
+export default function clearCache (key, value) {
+    //store cache
+}
